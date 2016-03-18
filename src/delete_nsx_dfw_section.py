d156300761566c21298e5f0edf6ef6361219c04f
Humair Ahmed <hue@humairahmed.com>
2016-03-17 17:59:24 -0700

    Update delete_nsx_dfw_section.py
    Updated exception. Removed httplib import.

--- src/delete_nsx_dfw_section.py
+++ src/delete_nsx_dfw_section.py
@@ -3,13 +3,12 @@
 #Program:        delete_nsx_dfw_section.py 
 #Description:    Deletes a  VMware NSX DFW section
 #Version:        1.0
-#Date:           01/01/2016
+#Date:           01/01/2016, updated 03/17/2015
 #Website:        http://www.HumairAhmed.com
 #Lead Developer: Humair Ahmed
 
 
 import requests
-import httplib
 
 url='https://10.100.1.72/api/4.0/firewall/globalroot-0/config/layer3sections/1025'
 nsxmanager_user='admin'
@@ -20,7 +19,7 @@ nsx_headers={'content-type':'application/xml'}
 
 try:
         response = requests.delete(url, headers=nsx_headers,auth=(nsxmanager_user,nsxmanager_password), verify=False)
-except httplib.IncompleteRead as e:
-        response = e.partial
+except requests.exceptions.ConnectionError as e:
+        print "Connection error!"
 
 print response.text
