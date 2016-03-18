ab31881fc3a2f6f94dbe688df81c53757ee9c03b
Humair Ahmed <hue@humairahmed.com>
2016-03-17 17:54:18 -0700

    Update default_nsx_dfw.py
    Updated exception.

--- src/default_nsx_dfw.py
+++ src/default_nsx_dfw.py
@@ -2,14 +2,13 @@
 
 #Program:        default_nsx_dfw.py
 #Description:    Resets VMware NSX DFW to default state
-#Version:        1.0
-#Date:           12/31/2015
+#Version:        1.1
+#Date:           12/31/2015, updated 03/17/2016
 #Website:        http://www.HumairAhmed.com
 #Lead Developer: Humair Ahmed
 
 
 import requests
-import httplib
 
 url='https://10.100.1.72/api/4.0/firewall/globalroot-0/config'
 nsxmanager_user='admin'
@@ -19,7 +18,7 @@ nsx_headers={'content-type':'application/xml'}
 
 try:
         response = requests.delete(url, headers=nsx_headers,auth=(nsxmanager_user,nsxmanager_password), verify=False)
-except httplib.IncompleteRead as e:
-        response = e.partial
+except requests.exceptions.ConnectionError as e:
+        print "Connection error!"
 
 print response.text
